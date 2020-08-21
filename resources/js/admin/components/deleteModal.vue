d6a81417d01848d46957b51b233ebb780213d291
Santosh <gudla.santosh3@gmail.com>
2020-08-21 19:05:39 +0530

    Reusable Delete modals for tags and categories

---
+++ resources/js/admin/components/deleteModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <!-- delete modal -->
+        <Modal :value="getDeleteModalObj.showDeleteModal" 
+        :mask-closable="false"
+        :closable="false"
+        width="360">
+            <p slot="header" style="color:#f60;text-align:center">
+                <Icon type="ios-information-circle"></Icon>
+                <span>Delete confirmation</span>
+            </p>
+            <div style="text-align:center">
+                <p>Are you sure you want to delete this category?</p>
+            </div>
+            <div slot="footer">
+                 <Button type="default" size="large" @click="closeModal">Close</Button>
+                <Button type="error" size="large" :loading="isDeleting" :disabled="isDeleting" @click="deleteCategory">Delete</Button>
+            </div>
+        </Modal>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+    data(){
+        return {
+            isDeleting: false,
+        }
+    },
+    methods: {
+        async deleteCategory() {
+			// var categoryObj = this.deleteData;
+			this.isDeleting = true
+			const res = await this.callApi('post', this.getDeleteModalObj.deleteUrl, this.getDeleteModalObj.data) //categoryObj
+			if (res.status == 200) {
+				// this.categories.splice(categoryObj.index,1);
+				// this.data.iconImage = categoryObj.iconImage
+				// this.deleteImage();
+				// this.deleteModal = false;
+                this.s('Category has been deleted successfully')
+                this.$store.commit('setDeleteModal', true)
+			} else { 
+                this.swr()
+                this.$store.commit('setDeleteModal', false)
+            }
+            this.isDeleting = false
+        },
+        closeModal(){
+            this.$store.commit('setDeleteModal', false)
+        }
+    },
+    computed: {
+        ...mapGetters(['getDeleteModalObj'])
+    }
+    
+}
+</script>
